aac6af666ba6f68429b625f18bc6fe1556373ae6
iya873 <siyana873@gmail.com>
2022-06-02 15:43:55 -0400

    show view show req.params.id

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
 app.get('/pokemon', (req, res) => {
     res.render('Index', {pokemon: pokeData})
 })
+
+app.get('/pokemon/:id', (req, res) => {
+    res.send(req.params.id)
+})
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
