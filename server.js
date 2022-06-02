746686c0908b20ce99b78f50334f8fa999d19c7f
iya873 <siyana873@gmail.com>
2022-06-02 15:42:05 -0400

    i can view a list of all my pokemon in the browser

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 const pokemon = require('./pokemon')
+const pokeData = require('./pokemon')
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App')
 })
 
 app.get('/pokemon', (req, res) => {
-    res.render('Index')
+    res.render('Index', {pokemon: pokeData})
 })
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
-
+const pokemon = require('../pokemon')
 const body = {
-    background: 'linear-gradient(35deg, yellow, blue) no-repeat'
+    background: 'yellow'
 }
 const logo = {
     width: '40%',
@@ -14,13 +14,27 @@ const header = {
 
 class Index extends React.Component {
     render() {
+        const {pokemon} = this.props;
         return (
             <>
                 <body style={body}>
                     <header style={header}>
-                        <img style={logo} src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png'></img>
                     <h1>See All The Pokemon!</h1>
+                        <img style={logo} src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png'></img>
                     </header>
+                    <div>
+                        {pokemon.map((pokemon, props) => {
+                            return (
+                                <ul>
+                                <li>{pokemon.name.toUpperCase()}</li>
+                                </ul>
+                                )
+                            })}
+                    </div>
+
+                <footer>
+                    Created by Nekked Hippie
+                </footer>
                 </body>
             </>
         )
