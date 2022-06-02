2869d19f4e508c0f5475e6c2a77d1c0f7fa318f7
iya873 <siyana873@gmail.com>
2022-06-02 14:53:36 -0400

    my server is up and running

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express()
 const port = 3000
 
 
-
+app.get('/', (req, res) => {
+    res.send('Welcome to the Pokemon App')
+})
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
